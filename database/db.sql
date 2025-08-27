073dec4096c244ff6ad9bb5b73e32ab5a8eb6d81
CaioGGR <caioggr@gmail.com>
2025-08-26 23:04:45 -0300

    ajustando script do db

--- database/db.sql
+++ database/db.sql
@@ -1,19 +1,8 @@
--- to create a new database
-CREATE DATABASE nodejs2;
+USE nodejs2;
 
--- to use database
-use nodejs2;
-
--- creating a new table
-CREATE TABLE customer (
+CREATE TABLE IF NOT EXISTS customer (
   id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(50) NOT NULL,
   address VARCHAR(100) NOT NULL,
   phone VARCHAR(15)
 );
-
--- to show all tables
-show tables;
-
--- to describe table
-describe customer;
